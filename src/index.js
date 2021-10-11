4a022ef99df9e764e2831eae9ff3a401401acaa4
soaresmilton <eng.milton.soares@gmail.com>
2021-10-11 18:41:55 -0300

    Readme Update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  res.end('<h1>Hello World!!</h1>')
+});
+
+server.listen(3000, () => console.log('Server is running at http://localhost:3000'))
