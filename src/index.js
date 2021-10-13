6ee66cf0495126ec0160158dbf4f2798bab9a2b6
soaresmilton <eng.milton.soares@gmail.com>
2021-10-13 17:28:28 -0300

    Criando primeira rota da aplicaçao

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const http = require('http');
+const users = require('./mocks/users');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
 
-  res.end('<h1>Hello World!!</h1>')
+  console.log(`Request method: ${req.method} | Endpoint: ${req.url}`);
+
+  if (req.url === '/users' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(users));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end(`Cannon ${req.method}  ${req.url}`);
+  }
+
 });
 
-server.listen(3000, () => console.log('Server is running at http://localhost:3000'))
+server.listen(3000, () => console.log('Server is running at http://localhost:3000'));
---
+++ src/mocks/users.js
@@ -0,0 +1,29 @@
+module.exports = [
+  {
+    id: 1, 
+    name: 'Milton Soares',
+    age: 25,
+    phone: '35998571018',
+    email: 'eng.milton.soares@gmail.com',
+    occupation : 'Software developer',
+    preferences: ['Books', 'RPG Games', 'Code', 'Movies']
+  },
+  {
+    id: 2, 
+    name: 'Sara Guimarães',
+    age: 22,
+    phone: '35998571018',
+    email: 'sara@gmail.com',
+    occupation : 'Nutricionist',
+    preferences: ['Books', 'Comedy Series', 'Movies']
+  },
+  {
+    id: 3, 
+    name: 'Andrea',
+    age: 41,
+    phone: '35998571018',
+    email: 'andrea@gmail.com',
+    occupation : 'Pharmacist',
+    preferences: ['Books', 'Sleep']
+  }
+]
